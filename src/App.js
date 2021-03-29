5fd54a759c0075d20fbda291c5b4daa17b60452a
Matheus <mmathias2011@outlook.com>
2021-03-29 17:39:43 -0300

    retirando Section para teste de deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Contato from './Components/Contato';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
-import SectionCurriculo from '../src/Components/SectionCurriculo';
+
 import Skills from './Components/Skills';
 import Sobre from './Components/Sobre'
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Header />
-    <SectionCurriculo />
+    
     <Sobre />
     <Skills />
     <Contato />
